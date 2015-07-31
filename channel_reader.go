2c441d23707908cc4a7f5ae2e90c9022e71cfbee
Oliver, Jonathan <git@jonathanoliver.com>
2015-07-31 15:56:33 -0600

    Implementing channel reader loop behavior.

--- channel_reader.go
+++ channel_reader.go
@@ -4,8 +4,11 @@ type ChannelReader struct {
 	controller       Controller
 	queue            string
 	bindings         []string
+	control          chan interface{}
 	acknowledgements chan interface{}
 	deliveries       chan Delivery
+	shutdown         bool
+	deliveryCount    uint64
 }
 
 func newReader(controller Controller, queue string, bindings []string) *ChannelReader {
@@ -13,15 +16,61 @@ func newReader(controller Controller, queue string, bindings []string) *ChannelR
 		controller:       controller,
 		queue:            queue,
 		bindings:         bindings,
+		control:          make(chan interface{}, 32),
 		acknowledgements: make(chan interface{}, 1024),
 		deliveries:       make(chan Delivery, 1024),
 	}
 }
 
 func (this *ChannelReader) Listen() {
+	acknowledger := newAcknowledger(this.control, this.acknowledgements)
+	go acknowledger.Listen()
+
+	for this.listen() {
+	}
+
+	this.controller.removeReader(this)
+}
+func (this *ChannelReader) listen() bool {
+	channel := this.controller.openChannel()
+	if channel == nil {
+		return false // broker no longer allowed to give me a channel, it has been manually closed
+	}
+
+	subscription := this.subscribe(channel)
+
+	for element := range this.control {
+		switch item := element.(type) {
+		case shutdownRequested:
+			this.shutdown = true
+			subscription.Close()
+		case subscriptionClosed:
+			this.deliveryCount += item.Deliveries
+			if this.shutdown {
+				// keep channel alive and gracefully stop acknowledgement
+				this.acknowledgements <- subscriptionClosed{Deliveries: this.deliveryCount}
+				this.deliveryCount = 0
+			} else {
+				// channel.Close() // channel failure; reconnect
+				// // time.Sleep(1second)???
+				// return true
+			}
+		case acknowledgementCompleted:
+			channel.Close() // we don't need the channel anymore
+			return false    // the shutdown process for this reader is complete
+		}
+	}
+
+	return true
+}
+func (this *ChannelReader) subscribe(channel Channel) *Subscription {
+	subscription := newSubscription(channel, this.queue, this.bindings, this.control, this.deliveries)
+	go subscription.Listen()
+	return subscription
 }
 
 func (this *ChannelReader) Close() {
+	this.control <- shutdownRequested{}
 }
 
 func (this *ChannelReader) Deliveries() <-chan Delivery {
