45fbcbdf4a287a260317567215ada7b840167a69
Oliver, Jonathan <git@jonathanoliver.com>
2015-08-11 16:48:33 -0600

    Pipeline channels are directional.

--- handlers/deserialization_handler.go
+++ handlers/deserialization_handler.go
@@ -3,12 +3,12 @@ package handlers
 import "github.com/smartystreets/pipeline/messaging"
 
 type DeserializationHandler struct {
-	input        chan messaging.Delivery
-	output       chan messaging.Delivery
+	input        <-chan messaging.Delivery
+	output       chan<- messaging.Delivery
 	deserializer Deserializer
 }
 
-func NewDeserializationHandler(input, output chan messaging.Delivery, deserializer Deserializer) *DeserializationHandler {
+func NewDeserializationHandler(input <-chan messaging.Delivery, output chan<- messaging.Delivery, deserializer Deserializer) *DeserializationHandler {
 	return &DeserializationHandler{
 		input:        input,
 		output:       output,
