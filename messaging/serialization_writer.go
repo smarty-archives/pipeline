0bed04eb12ceccfd0e4184af75cba9fe45349150
Oliver, Jonathan <git@jonathanoliver.com>
2015-08-13 17:53:53 -0600

    Implemented SerializationWriter.

--- messaging/serialization_writer.go
+++ messaging/serialization_writer.go
@@ -1,11 +1,89 @@
 package messaging
 
+import (
+	"reflect"
+	"strings"
+)
+
 type SerializationWriter struct {
-	serializer Serializer
+	writer            Writer
+	commitWriter      CommitWriter
+	serializer        Serializer
+	messageTypePrefix string
 }
 
-func NewSerializationWriter(serializer Serializer) *SerializationWriter {
+func NewSerializationWriter(inner Writer, serializer Serializer, messageTypePrefix string) *SerializationWriter {
+	commitWriter, _ := inner.(CommitWriter)
 	return &SerializationWriter{
-		serializer: serializer,
+		writer:            inner,
+		commitWriter:      commitWriter,
+		serializer:        serializer,
+		messageTypePrefix: messageTypePrefix,
 	}
+}
+
+func (this *SerializationWriter) Write(dispatch Dispatch) error {
+	if len(dispatch.Payload) > 0 && len(dispatch.MessageType) > 0 {
+		return this.writer.Write(dispatch) // already have a payload a message type, forward to inner
+	}
+
+	if len(dispatch.Payload) == 0 && dispatch.Message == nil {
+		return EmptyDispatchError // no payload and no message, this is a total fail
+	}
+
+	if dispatch.Message == nil && len(dispatch.MessageType) == 0 {
+		return MessageTypeDiscoveryError // no message type and no way to get it
+	}
+
+	if payload, err := this.serializer.Serialize(dispatch.Message); err != nil {
+		return err // serialization failed
+	} else {
+		dispatch.Payload = payload
+	}
+
+	if len(dispatch.MessageType) == 0 {
+		dispatch.MessageType = discoverType(this.messageTypePrefix, dispatch.Message)
+	}
+
+	// TODO
+	// if len(dispatch.MessageType) == 0 {
+	// 	return errors.New("Unable to write message, the type cannot be discovered.")
+	// }
+
+	return this.writer.Write(dispatch)
+}
+
+// TODO: should this be pulled out into its own stucture?
+// that would allow different behaviors to be invoked
+func discoverType(prefix string, message interface{}) string {
+	reflectType := reflect.TypeOf(message)
+	if name := reflectType.Name(); len(name) > 0 {
+		return prefix + strings.ToLower(name)
+	}
+
+	name := reflectType.String()
+	index := strings.LastIndex(name, ".")
+
+	if index == -1 {
+		return ""
+	}
+
+	suffix := strings.ToLower(name[index+1:])
+	if strings.HasPrefix(name, "*") {
+		return "*" + prefix + suffix
+	} else {
+		return prefix + suffix
+	}
+}
+
+func (this *SerializationWriter) Commit() error {
+	if this.commitWriter == nil {
+		return nil
+	}
+
+	return this.commitWriter.Commit()
+}
+
+func (this *SerializationWriter) Close() {
+	this.writer.Close()
 }
--- messaging/serialization_writer_test.go
+++ messaging/serialization_writer_test.go
@@ -1,36 +1,218 @@
 package messaging
 
 import (
+	"errors"
 	"io/ioutil"
 	"log"
+	"os"
+	"time"
 
+	"github.com/smartystreets/assertions/should"
 	"github.com/smartystreets/gunit"
 )
 
 type SerializationWriterFixture struct {
 	*gunit.Fixture
 
-	writer     *SerializationWriter
-	serializer *FakeSerializer
+	writer            *SerializationWriter
+	inner             *FakeCommitWriter
+	serializer        *FakeSerializer
+	messageTypePrefix string
 }
 
 func (this *SerializationWriterFixture) Setup() {
 	log.SetOutput(ioutil.Discard)
+
+	this.inner = &FakeCommitWriter{}
 	this.serializer = &FakeSerializer{}
-	this.writer = NewSerializationWriter(this.serializer)
+	this.messageTypePrefix = "message.type.prefix."
+	this.buildWriter()
+}
+func (this *SerializationWriterFixture) Teardown() {
+	log.SetOutput(os.Stdout)
 }
 
-// TODO: does this writer receive a Dispatch or a Delivery? If, like all other
-// writers it receives a Dispatch, how does it get the actual Message? (which
-// is on the Delivery...)
+func (this *SerializationWriterFixture) buildWriter() {
+	this.writer = NewSerializationWriter(this.inner, this.serializer, this.messageTypePrefix)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestWriterAddsSerializedPayloadAndTypeToDispatch() {
+	this.inner.writeError = errors.New("ensure inner errors are returned to caller")
+
+	original := Dispatch{
+		SourceID:    1,
+		MessageID:   2,
+		Destination: "3",
+		MessageType: "", // should be populated
+		Encoding:    "4",
+		Durable:     true,
+		Expiration:  time.Now(),
+		Payload:     nil,
+		Message:     TestMessage{},
+	}
+
+	err := this.writer.Write(original)
+
+	this.So(err, should.Equal, this.inner.writeError)
+	this.So(this.inner.written, should.NotBeEmpty)
+	this.So(this.inner.written[0], should.Resemble, Dispatch{
+		SourceID:    1,
+		MessageID:   2,
+		Destination: "3",
+		MessageType: this.messageTypePrefix + "testmessage",
+		Encoding:    "4",
+		Durable:     true,
+		Expiration:  original.Expiration,
+		Payload:     testSerializedPayload,
+		Message:     TestMessage{},
+	})
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestSerializationFails() {
+	this.serializer.serializeError = errors.New("Serialization failed")
+	this.buildWriter()
+
+	err := this.writer.Write(Dispatch{Message: TestMessage{}})
+	this.So(err, should.Equal, this.serializer.serializeError)
+	this.So(this.inner.written, should.BeEmpty)
+}
+
+func (this *SerializationWriterFixture) TestDispatchAlreadyContainsSerializedPayload() {
+	this.inner.writeError = errors.New("ensure inner errors are returned to caller")
+
+	message := Dispatch{
+		MessageType: "untouched",
+		Payload:     []byte("already serialized"),
+	}
+	err := this.writer.Write(message)
+	this.So(err, should.Equal, this.inner.writeError)
+	this.So(this.serializer.called, should.Equal, 0)
+	this.So(this.inner.written, should.Resemble, []Dispatch{message})
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestDispatchAlreadyContainsMessageType() {
+	message := Dispatch{
+		MessageType: "untouched",
+		Message:     TestMessage{},
+	}
+	this.writer.Write(message)
+	this.So(this.inner.written, should.Resemble, []Dispatch{Dispatch{
+		MessageType: "untouched",
+		Payload:     testSerializedPayload,
+		Message:     TestMessage{},
+	}})
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestDispatchToMessagePointer() {
+	message := Dispatch{
+		Message: &TestMessage{},
+	}
+	this.writer.Write(message)
+	this.So(this.inner.written, should.Resemble, []Dispatch{Dispatch{
+		MessageType: "*" + this.messageTypePrefix + "testmessage",
+		Payload:     testSerializedPayload,
+		Message:     &TestMessage{},
+	}})
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestWriteEmptyDispatch() {
+	err := this.writer.Write(Dispatch{})
+	this.So(err, should.Equal, EmptyDispatchError)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestWriteEmptyMessageType() {
+	err := this.writer.Write(Dispatch{Payload: []byte("already serialized")})
+	this.So(err, should.Equal, MessageTypeDiscoveryError)
+	this.So(this.inner.written, should.BeEmpty)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestCommitInvokesUnderlyingWriter() {
+	this.inner.commitError = errors.New("commit error")
+	err := this.writer.Commit()
+	this.So(this.inner.commits, should.Equal, 1)
+	this.So(err, should.Equal, this.inner.commitError)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestCommitOnRegularWriterPanics() {
+	this.writer = NewSerializationWriter(&FakeWriter{}, this.serializer, this.messageTypePrefix)
+	err := this.writer.Commit()
+	this.So(err, should.BeNil)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (this *SerializationWriterFixture) TestCloseUnderlyingWriter() {
+	this.writer.Close()
+	this.So(this.inner.closed, should.Equal, 1)
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type FakeWriter struct{}
+
+func (this *FakeWriter) Write(dispatch Dispatch) error { return nil }
+func (this *FakeWriter) Close()                        {}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type FakeCommitWriter struct {
+	written     []Dispatch
+	commits     int
+	closed      int
+	writeError  error
+	commitError error
+}
+
+func (this *FakeCommitWriter) Write(dispatch Dispatch) error {
+	this.written = append(this.written, dispatch)
+	return this.writeError
+}
+
+func (this *FakeCommitWriter) Commit() error {
+	this.commits++
+	return this.commitError
+}
+
+func (this *FakeCommitWriter) Close() {
+	this.closed++
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
 type FakeSerializer struct {
-	called int
+	called         int
+	serializeError error
 }
 
 func (this *FakeSerializer) Serialize(interface{}) ([]byte, error) {
 	this.called++
-	return []byte("X"), nil
+	if this.serializeError != nil {
+		return nil, this.serializeError
+	} else {
+		return testSerializedPayload, nil
+	}
 }
+
+var testSerializedPayload = []byte("serializer called successfully")
+
+////////////////////////////////////////////////////////////////////////////////
+
+type TestMessage struct{}
+
+////////////////////////////////////////////////////////////////////////////////
