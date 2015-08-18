c1ce729e8de2b563d90a6d55d111d74f11e098f6
Oliver, Jonathan <git@jonathanoliver.com>
2015-08-17 20:28:55 -0600

    Removed unused interfaces.

--- projector/interfaces.go
+++ projector/interfaces.go
@@ -3,34 +3,14 @@ package projector
 import "time"
 
 type (
-	RawMessage struct { // TODO: remove?
-		Payload         []byte
-		MessageType     string
-		Acknowledgement DeliveryReceipt
-	}
-
-	TransformationMessage struct {
-		Message         interface{}
-		Acknowledgement DeliveryReceipt
-		Now             time.Time
-	}
-
 	DocumentMessage struct {
 		Documents []Document
 		Receipt   interface{}
-	}
-
-	DeliveryReceipt interface { // TODO: remove
-		Acknowledge()
 	}
 
 	Document interface {
 		Lapse(now time.Time) (next Document)
 		Handle(message interface{}) bool
 		Path() string
-	}
-
-	Listener interface {
-		Listen()
 	}
 )
