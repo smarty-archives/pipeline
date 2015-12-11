537edc06867519bdfec674f6859724e6f84e9e3a
Oliver, Jonathan <git@jonathanoliver.com>
2015-12-11 10:57:03 -0700

    Removed notes about implementing behavior that has been completed.

--- handlers/message_handler.go
+++
@@ -1,9 +0,0 @@
-package handlers
-
-// func (this *?) listener() {
-// 	for delivery := range this.input {
-// 		this.app.Handle(delivery.Message)
-// 		this.output <- delivery.Receipt
-// 	}
-// 	close(this.output)
-// }
